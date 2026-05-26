beebacedaeb1b7d8499bce9af0cd0db5c7d23e7c
Fiona Spencer <fionagspencer@gmail.com>
2026-05-26 15:41:06 -0400

    Initial Commit

---
+++ SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Fiona Spencer on 2026-05-26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
